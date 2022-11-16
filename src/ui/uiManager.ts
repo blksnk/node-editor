adf0e86e281cb1affef8cea6ebca2830119604ca
Jean-Nicolas Veigel <art.jnveigel@gmail.com>
2022-11-16 15:13:49 +0100

    :construction: :lipstick: create right panel in ui manager

--- src/ui/uiManager.ts
+++ src/ui/uiManager.ts
@@ -2,12 +2,16 @@ import { TabName, UIManagerOptions } from './uiManager.types';
 import { createUiRoot } from './uiRoot';
 import { createNodePanel } from './panels/nodes';
 import { Runtime } from '../runtime/runtime';
+import { createRightPanel } from './panels/rightPanel';
+import { RuntimeOutput } from '../runtime/runtime.types';
+import { OutputsList } from './panels/tabs/outputs';
 
 export class UIManager {
   root: HTMLElement;
   target: UIManagerOptions['target'];
   activeTabName: TabName = TabName.inspect;
   nodesPanel: HTMLElement;
+  rightPanel: ReturnType<typeof createRightPanel>;
   runtime: Runtime;
 
   constructor(options: UIManagerOptions) {
@@ -17,12 +21,22 @@ export class UIManager {
     this.nodesPanel = createNodePanel(
       this.runtime.createNode.bind(this.runtime),
     );
-
+    this.rightPanel = createRightPanel({
+      outputs: this.runtime.outputs,
+    });
     this.attachUI();
   }
 
-  attachUI() {
+  public update(data: { outputs?: RuntimeOutput[] }) {
+    if (data.outputs) {
+      console.log(data.outputs);
+      OutputsList.refresh(this.rightPanel.tabs.outputTab.list, data.outputs);
+    }
+  }
+
+  private attachUI() {
     this.target.appendChild(this.root);
     this.root.appendChild(this.nodesPanel);
+    this.root.appendChild(this.rightPanel.panel);
   }
 }
